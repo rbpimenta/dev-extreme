c0a4428a6d139b78ada5c03ecbd6cd278740aa28
Rodrigo Pimenta <rodrigopimenta2@gmail.com>
2019-10-10 00:44:37 -0300

    Inclusão de dxi-item, utilizando um template genérico para a exibição das Posições (incluindo um label + dx-button + dx-select-box)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Employee, Service} from './app.service';
 
-@Component({
+@Component ({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [Service],
 })
 export class AppComponent {
-  title = 'angular-cli';
+
+  employee: Employee;
+  positions: string[];
+  rules: Object;
+  private editorOptions: any;
+
+  constructor(service: Service) {
+    this.employee = service.getEmployee();
+    this.positions = service.getPositions();
+
+    this.editorOptions = {
+      items: this.positions
+    };
+
+    this.rules = { 'X': /[02-9]/ };
+  }
+
+  alert() {
+    window.alert('teste');
+  }
 }
